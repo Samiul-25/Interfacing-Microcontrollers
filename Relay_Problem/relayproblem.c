b04fb879c010326e5182e45b9466bd05a0dbd2c3
Md.Samiul Alim <53310538+Samiul-25@users.noreply.github.com>
2023-12-06 19:07:21 +0600

    Add files via upload

---
+++ Relay_Problem/relayproblem.c
@@ -0,0 +1,16 @@
+void main() {
+     TRISB = 0x00;
+     portb = 0x00;
+
+
+     while(1)
+     {
+             portb.f0 = 1;
+             portb.f7 = 0;
+             delay_ms(1000);
+             portb.f0 = 0;
+             portb.f7 = 1;
+             delay_ms(1000);
+     }
+
+}
---
+++ Relay_Problem/relayproblem.cp
@@ -0,0 +1,17 @@
+#line 1 "C:/Users/Samiul Alim/Desktop/Lab/Relay_Problem/relayproblem.c"
+void main() {
+ TRISB = 0x00;
+ portb = 0x00;
+
+
+ while(1)
+ {
+ portb.f0 = 1;
+ portb.f7 = 0;
+ delay_ms(1000);
+ portb.f0 = 0;
+ portb.f7 = 1;
+ delay_ms(1000);
+ }
+
+}
